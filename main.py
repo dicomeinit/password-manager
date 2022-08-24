9afab8fa6a7963d9c3ab7d8b0c371d797c1d5ed0
Diana <dicomeinit@gmail.com>
2022-08-24 16:40:53 +0300

    Generate a Password & Copy it to the Clipboard

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from tkinter import *
 from tkmacosx import Button
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    entry_password.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save():
-
     website = entry_website.get()
     email = entry_email_uname.get()
     password = entry_password.get()
@@ -16,7 +39,7 @@ def save():
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
-                           f"\nPassword: {password} \nIs it ok to save?")
+                                                              f"\nPassword: {password} \nIs it ok to save?")
 
         if is_ok:
             with open("data.txt", "a") as data_file:
@@ -25,13 +48,12 @@ def save():
                 entry_password.delete(0, END)
 
 
-
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
-
 
 canvas = Canvas(width=200, height=200)
 password_image = PhotoImage(file="logo.gif")
@@ -64,11 +86,10 @@ entry_password.grid(column=1, row=3, sticky="EW")
 
 # Buttons
 
-generate_btn = Button(window, text="Generate Password")
+generate_btn = Button(window, text="Generate Password", command=generate_password)
 generate_btn.grid(column=2, row=3, sticky="EW")
 
 add_btn = Button(text="Add", width=35, command=save)
 add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
-
 
 window.mainloop()
